677a4d1bfcbec4a8b2fdecfa0aa4e31f391d5d0b
Charles Pernet <charlespernet@gmail.com>
2016-06-15 17:45:29 +0200

    edit update aupair

--- app/controllers/aupairs_controller.rb
+++ app/controllers/aupairs_controller.rb
@@ -1,7 +1,22 @@
 class AupairsController < ApplicationController
+  before_action :set_aupair, only: [:edit, :update]
+
   def edit
   end
 
   def update
+    @user.update(aupair_params)
+    flash[:notice] = "Profil mis à jour"
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def set_aupair
+    @user = Aupair.find(params[:id])
+  end
+
+  def aupair_params
+    params.require(:aupair).permit(:first_name,:last_name, :birth_date, :country_code, :description, :driving_license, :smoker, :musician)
   end
 end
--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,15 @@
 class FamiliesController < ApplicationController
+  before_action :set_family, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_family
+    @user = Family.find(params[:id])
+  end
 end
