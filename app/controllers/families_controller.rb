ca4750c387d9fd02c6dad63d80dfd17804cc0743
Charles Pernet <charlespernet@gmail.com>
2016-06-15 17:49:21 +0200

    edit update family

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -5,11 +5,18 @@ class FamiliesController < ApplicationController
   end
 
   def update
+    @user.update(family_params)
+    flash[:notice] = "Profil mis à jour"
+    redirect_to dashboard_path
   end
 
   private
 
   def set_family
     @user = Family.find(params[:id])
+  end
+
+  def family_params
+    params.require(:family).permit(:name, :country_code, :smoker, :domestic_animal)
   end
 end
