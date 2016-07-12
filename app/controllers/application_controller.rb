0a68df3846db1ab0b800fca8613f4c2886e6f3ed
Charles Pernet <charlespernet@gmail.com>
2016-07-12 11:07:39 +0200

    test http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  if Rails.env.staging?
+    http_basic_authenticate_with name: "butrfly", password: "butrfly"
+  end
+
   before_action :authenticate_user!
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
