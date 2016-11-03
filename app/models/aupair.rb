1f3ea4eb3654900fc8a50ad782a08d286a80c807
aurelien galoppe <agaloppe@me.com>
2016-11-03 19:09:05 +0100

    photos problems fixed

--- app/models/aupair.rb
+++ app/models/aupair.rb
@@ -7,7 +7,7 @@ class Aupair < ActiveRecord::Base
     dispo_from
     stay_duration_cd)
 
-  has_attachment :photo
+  has_attachments :photos, maximum: 3
   acts_as :user
   has_many :spoken_languages
   has_many :languages, through: :spoken_languages
